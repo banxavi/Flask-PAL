bfd1ee2d66355b8c31b03e68e97dfe554f69a6f5
banxavi <banxavi@gmail.com>
2022-01-19 09:24:00 +0700

    comment

--- main.py
+++ main.py
@@ -70,23 +70,25 @@ def emp_email(email):
 		
 @app.route('/add', methods=['POST'])
 def add_emp():
-	
+	# Json : JavaSript Object Notation, convert json string to handle data by JavaScript 
 	_json = request.json
 	_name = _json['name']
 	_email = _json['email']
 	_phone = _json['phone']
 	_address = _json['address']	
-	_password = _json['password']		
-	if _name and _email and _phone and _address and _password and request.method == 'POST':			
-		sqlQuery = "INSERT INTO rest_emp(name, email, password, phone, address) VALUES(%s, %s, %s, %s, %s)"
-		bindData = (_name, _email, _password, _phone, _address)
-		conn = mysql.connect()
-		cursor = conn.cursor()
-		cursor.execute(sqlQuery, bindData)
-		conn.commit()
-		respone = jsonify('Employee added successfully!')
-		respone.status_code = 200
-		return respone
+	_password = _json['password']	
+	_image = _json['image']	
+
+	if _name and _email and _phone and _address and _password and _image and request.method == 'POST':			
+		sqlQuery = "INSERT INTO rest_emp(name, email, password, phone, address, image) VALUES(%s, %s, %s, %s, %s, %s)" # Query Mysql
+		bindData = (_name, _email, _password, _phone, _address, _image) # Data input (tuple)
+		conn = mysql.connect()	# Connect to mysql server
+		cursor = conn.cursor()	# used to execute statements to communicate with the MySQL database ( similar libraries)
+		cursor.execute(sqlQuery, bindData) # Excecute query
+		conn.commit()  #  provides the database confirmation regarding the changes made by a user or an application in the database.
+		respone = jsonify('Employee added successfully!') # Show them when test on postman
+		respone.status_code = 200 # set status
+		return respone # Return object json
 	else:
 		return not_found()
 
@@ -100,10 +102,11 @@ def update_emp():
 	_email = _json['email']
 	_phone = _json['phone']
 	_address = _json['address']
+	_image = _json['image']	
 	# validate the received values
-	if _name and _email and _phone and _address and _id and request.method == 'PUT':			
-		sqlQuery = "UPDATE rest_emp SET name=%s, email=%s, phone=%s, address=%s WHERE id=%s"
-		bindData = (_name, _email, _phone, _address, _id,)
+	if _name and _email and _phone and _address and _id and _image and request.method == 'PUT':			
+		sqlQuery = "UPDATE rest_emp SET name=%s, email=%s, phone=%s, address=%s, image=%s WHERE id=%s"
+		bindData = (_name, _email, _phone, _address, _id,_image,)
 		conn = mysql.connect()
 		cursor = conn.cursor()
 		cursor.execute(sqlQuery, bindData)
